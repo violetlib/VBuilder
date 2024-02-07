9f766f4156e89522be66f63b6394ef0d0685aa26
Alan Snyder <ranchlens@cbfiddle.com>
2024-02-06 18:15:35 -0800

    Add header comments

--- ant/src/org/violetlib/vbuilder/ant/AntBuildDelegate.java
+++ ant/src/org/violetlib/vbuilder/ant/AntBuildDelegate.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024 Alan Snyder.
+ * All rights reserved.
+ *
+ * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
+ * accompanying license terms.
+ */
+
 package org.violetlib.vbuilder.ant;
 
 import org.violetlib.vbuilder.BuildDelegate;
